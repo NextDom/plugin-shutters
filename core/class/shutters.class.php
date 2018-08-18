3b7c81fe436b7c7ef05a18c5098c0baefab11139
Unknown <seki72000@gmail.com>
2018-08-18 13:22:41 +0200

    Ajout commandes objet infos externes

--- core/class/shutters.class.php
+++ core/class/shutters.class.php
@@ -19,7 +19,7 @@
 
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-require_once 'shuttersCmd.class.php';
+//require_once 'shuttersCmd.class.php';
 
 class shutters extends eqLogic
 {
@@ -360,6 +360,28 @@ class shutters extends eqLogic
       public static function preConfig_<Variable>() {
       }
      */
+
+    /*     * **********************Getteur Setteur*************************** */
+}
+
+class shuttersCmd extends cmd
+{
+    /*     * *************************Attributs****************************** */
+    /*     * ***********************Methode static*************************** */
+    /*     * *********************Methode d'instance************************* */
+    /*
+     * Non obligatoire permet de demander de ne pas supprimer les commandes 
+     même si elles ne sont pas dans la nouvelle configuration de l'équipement 
+     envoyé en JS
+      public function dontRemoveCmd() {
+      return true;
+      }
+     */
+
+    public function execute($_options = array())
+    {
+        
+    }
 
     /*     * **********************Getteur Setteur*************************** */
 }
